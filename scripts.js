f16d0eeec9639d435a2eb3863ee904adb51b29fa
rosemarie sutanto <rosemariesutanto@gmail.com>
2021-06-09 13:11:04 +0700

    implemented tickets form

---
+++ scripts.js
@@ -0,0 +1,22 @@
+window.onload = function(){
+    // your code
+  const modal = document.getElementById("myModal");
+
+  const btn = document.getElementById("sticky-buy-tickets");
+
+  const span = document.getElementsByClassName("close")[0];
+
+  btn.onclick = function() {
+    modal.style.display = "block";
+  }
+
+  span.onclick = function() {
+    modal.style.display = "none";
+  }
+
+  window.onclick = function(event) {
+    if (event.target == modal) {
+      modal.style.display = "none";
+  }
+}
+};
